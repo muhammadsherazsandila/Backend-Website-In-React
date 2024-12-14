4f8551d86e7cd1d4157a0dae1da1abde0db8b22d
muhammad sheraz <msd.sheraz046@gmail.com>
2024-12-14 17:47:03 +0500

    adding cors

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,10 @@ const workoutRoutes = require('./routes/mainRouter');
 const app = express();
 const cors = require('cors');
 
-app.use(cors())
+// Allow requests from your frontend origin
+app.use(cors({
+    origin: 'https://backend-website-in-react-frontend.vercel.app'
+}));
 app.use(express.urlencoded({extended : true}))
 app.use(express.json());
 require('dotenv').config();
